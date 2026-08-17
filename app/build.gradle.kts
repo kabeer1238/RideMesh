4694e3169c3e6862d17fe6bc8a87bc31b055f563
kabeer1238 <kabeersulfi@gmail.com>
2026-08-17 22:02:37 +0530

    Bump RideMesh to v0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bikemesh.ridemesh"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.3.1"
+        versionCode = 8
+        versionName = "0.3.2"
     }
 
     buildFeatures {
