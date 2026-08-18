153dbf6a4a8dd9c067f376413292b19ad62e7f96
kabeer1238 <kabeersulfi@gmail.com>
2026-08-18 23:13:42 +0530

    Beta 1.4: bump single-group branding release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bikemesh.ridemesh"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.4.6-beta1.2"
+        versionCode = 18
+        versionName = "0.4.8-beta1.4"
     }
 
     buildFeatures {
