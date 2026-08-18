0a362e19111b47bbfd07d6a499ad571901dd7fd5
kabeer1238 <kabeersulfi@gmail.com>
2026-08-18 20:09:23 +0530

    Bump RideMesh Beta 1.1 branding build to 0.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bikemesh.ridemesh"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.4.4-beta1.1"
+        versionCode = 15
+        versionName = "0.4.5-beta1.1"
     }
 
     buildFeatures {
