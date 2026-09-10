0c80fbfe107f32f2cae575f1d5fe26fdb285a7b0
kabeer1238 <kabeersulfi@gmail.com>
2026-09-10 17:46:56 +0530

    Add Concentus Opus dependency for offline voice

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
     implementation("com.google.zxing:core:3.5.4")
     implementation("io.github.webrtc-sdk:android:144.7559.09")
+    implementation("io.github.jaredmdobson:concentus:1.0.2")
 
     testImplementation("junit:junit:4.13.2")
 }
